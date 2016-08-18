5c0fda9cc1ae71df1d2751a5e9f434ec5e3373f1
Karel Majzlík <info@vmweb.cz>
2016-08-18 15:28:37 +0200

    New project script

--- addproject/new_user.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER 'demo'@'localhost' IDENTIFIED WITH mysql_native_password;
-GRANT USAGE ON *.* TO 'demo'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
-SET PASSWORD FOR 'demo'@'localhost' = PASSWORD('demo');
-GRANT ALL PRIVILEGES ON `demo\_%`.* TO 'demo'@'localhost';
---
+++ project.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+source common.sh
+
+domain=
+while [[ $proddomain = "" ]]; do
+  read -p "Insert domain without WWW (eg domain.com) " proddomain
+done
+
+read -p "Insert 3rd level subdomain (default = www) " subdomain
+subdomain=${subdomain:-www}
+
+defaultdevdomain="$( cut -d '.' -f 1 <<< "$proddomain" ).dev"
+read -p "Insert development domain (default=$defaultdevdomain) " devdomain
+devdomain=${devdomain:-$defaultdevdomain}
+
+projectname="$proddomain-$subdomain"
+projectdir="/var/www/drupal/$projectname"
+
+echo "Project name: $projectname"
+echo "Project dir: $projectdir"
+
+mkdir -p $projectdir
+cd $projectdir
+git init
+git remote add origin git@github.com:vmwebCZ/template-d8.git
+git pull origin master
+rm -rf .git
+git init
+
+mysqldb=
+while [[ $mysqldb = "" ]]; do
+  read -p "Insert database name, must contain underscore (eg customer_project) " mysqldb
+done
+
+cd $dir
+mysqluser="$( cut -d '_' -f 1 <<< "$mysqldb" )"
+mysqlcommand=$(cat project/new_user.sql | sed "s|USERNAME|$mysqluser|g");
+mysql -u root "-p$mysqlpass" -Bse "DROP USER IF EXISTS '$mysqluser'@'localhost';"
+mysql -u root "-p$mysqlpass" -Bse "$mysqlcommand"
+mysql -u root "-p$mysqlpass" -Bse "drop database if exists $mysqldb;"
+mysql -u root "-p$mysqlpass" -Bse "create database $mysqldb;"
+
+conffile="/etc/apache2/sites-available/030-$projectname.conf"
+sudo cp project/site.conf $conffile
+sudo sed -i "s|PROJECT|$projectname|g" $conffile
+sudo sed -i "s|DOMAIN|$devdomain|g" $conffile
+
+sudo a2ensite "030-$projectname"
+sudo service apache2 restart
+
+echo "127.0.0.1	$devdomain" | sudo tee -a /etc/hosts
+
+echo "All done."
+echo "You should now do:"
+echo "cd $projectdir"
+echo "phing init"
